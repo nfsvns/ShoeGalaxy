ed1dd71a3a77c25c4e04c888e5d6f6d24f2d3212
thankhou <nguyenthanhhau2809@gmail.com>
2023-10-06 17:05:46 +0700

    hau_updateadmin_i18n

--- src/main/java/com/poly/service/UserService.java
+++ src/main/java/com/poly/service/UserService.java
@@ -4,6 +4,7 @@ import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
