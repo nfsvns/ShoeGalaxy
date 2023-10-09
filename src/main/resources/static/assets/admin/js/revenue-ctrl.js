1b7f1a28b468a7efa8f1102af6d02f5c0fd0bb91
HieuPT <phamthieu961@gmail.com>
2023-10-09 19:20:08 +0700

    hieu: làm chart revenue

--- src/main/resources/static/assets/admin/js/revenue-ctrl.js
+++ src/main/resources/static/assets/admin/js/revenue-ctrl.js
@@ -1,52 +1,69 @@
 app.controller("revenue-ctrl", function($scope, $http, $location) {
-	
+
 	$scope.selectedYear = null;
-	$scope.initialize = function() {	
+	$scope.initialize = function() {
 		$http.get("/rest/revenue").then(resp => {
 			$scope.years = resp.data;
 			$scope.selectedYear = 2023; // Đặt năm 2023 làm năm mặc định
 			$scope.getRevenueByYear(); // Gọi hàm để tải dữ liệu doanh thu ngay khi trang tải lên
 		}).catch(error => {
-	    	$location.path("/unauthorized");
-	    })
+			$location.path("/unauthorized");
+		})
 	}
+	
 	$scope.getRevenueByYear = function() {
 		// Perform an API call to get revenue data for the selected year
 		// Replace 'YOUR_API_URL' with the actual API endpoint
 		$http.get("/rest/revenue/" + $scope.selectedYear)
 			.then(function(response) {
 				$scope.revenueItems = response.data;
+
+				// Tạo mảng dataPoints sau khi dữ liệu đã được tải xong
+				var dataPoints = [];
+				$scope.revenueItems.forEach(function(item) {
+					var xValue = item[0]; // Tháng
+					var yValue = item[1]; // Doanh thu
+					// Tạo một đối tượng chứa x và y và đẩy vào mảng dataPoints
+					dataPoints.push({
+						x: xValue,
+						y: yValue
+					});
+					console.log("x: " + xValue + ", y: " + yValue);
+				});
+				console.log(dataPoints);
+
+				var chart = new CanvasJS.Chart("chartContainer", {
+					animationEnabled: true,
+					exportEnabled: true,
+					theme: "light1", // "light1", "light2", "dark1", "dark2"
+					title: {
+						text: "Proceed in year"
+					},
+					axisY: {
+						includeZero: true,
+						prefix: "$", //prefix tiền tố, suffix hậu tố
+					},
+					axisX: {
+						interval: 1, // Chỉ hiển thị giá trị nguyên trên trục x
+						prefix: "Tháng ", //prefix tiền tố, suffix hậu tố
+
+					},
+					data: [{
+						type: "column", //change type to bar, line, area, pie, etc
+						indexLabel: "{y}", //Shows y value on all Data Points
+						indexLabelFontColor: "#5A5757", //màu
+						indexLabelFontSize: 16, //font
+						indexLabelPlacement: "outside", //Cho chữ ở đâu
+						dataPoints: dataPoints
+					}],
+				});
+				chart.render();
 			})
 			.catch(function(error) {
 				console.log('Error fetching revenue data:', error);
 			});
 	};
-		var chart = new CanvasJS.Chart("chartContainer", {
-			animationEnabled: true,
-			exportEnabled: true,
-			theme: "light1", // "light1", "light2", "dark1", "dark2"
-			title:{
-				text: "Proceed in year"
-			},
-			  axisY: {
-			  includeZero: true
-			},
-			data: [{
-				type: "column", //change type to bar, line, area, pie, etc
-				//indexLabel: "{y}", //Shows y value on all Data Points
-				indexLabelFontColor: "#5A5757",
-				  indexLabelFontSize: 16,
-				indexLabelPlacement: "outside",
-				dataPoints: [
-					{ x: 20, y: 55 },
-					{ x: 50, y: 92, indexLabel: "\u2605 Highest" },
-					{ x: 60, y: 68 },
-					{ x: 70, y: 38 },
-					{ x: 130, y: 21, indexLabel: "\u2691 Lowest" }
-				]
-			}]
-		});
-		chart.render();
-		$scope.initialize();
+
+	$scope.initialize();
 });
 
