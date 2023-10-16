03c1230a46e5f06f48c25ec8345ff58e49d2bec3
HieuPT <phamthieu961@gmail.com>
2023-10-16 20:40:14 +0700

    initial admin 2

--- src/main/resources/static/assets/admin2/js/admin-app.js
+++ src/main/resources/static/assets/admin2/js/admin-app.js
@@ -1,23 +1,21 @@
-app = angular.module("app1", ["ngRoute"]);
-
-/*app.run(function($http, $rootScope){
-	$http.get(`/rest/security/authentication`).then(resp => {
-		if(resp.data){
-			$auth = $rootScope.$auth = resp.data;
-			$http.defaults.headers.common["Authorization"] = $auth.token;
-		}
-	});
-})*/
+const app = angular.module("app1", ["ngRoute"]);
 
 app.config(function($routeProvider) {
 	$routeProvider
-		.when("/product", {
-			templateUrl: "/assets/admin/product/index.html",
-			controller: "product-ctrl"
-		})
-
-		.otherwise({
+		.when("/dashboard", {
 			templateUrl: "/assets/admin2/pages/dashboard/index.html",
+			controller: "dashboard-ctrl"
+		})
+		.when("/table", {
+			templateUrl: "/assets/admin2/pages/tables.html",
+			
+		})
+		.when("/billing", {
+			templateUrl: "/assets/admin2/pages/billing.html",
+			
+		})
+		.otherwise({
+			redirectTo: "/dashboard"
 
 		});
 });
---
+++ src/main/resources/static/assets/admin2/js/dashboard.js
@@ -0,0 +1,4 @@
+app.controller("dashboard-ctrl", function($scope, $http){
+	
+});
+
